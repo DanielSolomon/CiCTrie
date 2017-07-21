79a7ef40e97c3bdc6c2fb9d4b59b32d76d2a7ec2
warforgad <warforgad@gmail.com>
2017-07-21 17:52:00 +0300

    Added node types

---
+++ code/inc/nodes.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <stdint.h>
+
+// The maximun number of branches going out of a CNode. Must match the bitmap size.
+#define MAX_BRANCHES 32
+
+typedef struct main_node_t main_node_t;
+
+typedef enum 
+{
+    CNODE,
+    SNODE,
+    INODE,
+    TNODE,
+    LNODE
+} node_type_t;
+
+typedef struct
+{
+    int key;
+    int value;
+} snode_t;
+
+typedef struct 
+{
+    snode_t snode;
+} tnode_t;
+
+typedef struct lnode_t
+{
+    snode_t snode;
+    struct lnode_t* next;
+} lnode_t;
+
+typedef struct
+{
+    main_node_t* main;
+} inode_t;
+
+typedef struct
+{
+    node_type_t type;
+    union
+    {
+        inode_t inode;
+        snode_t snode;
+    } node;
+} branch_t;
+
+typedef struct
+{
+    uint32_t bmp;
+    branch_t* array[MAX_BRANCHES];
+} cnode_t;
+
+struct main_node_t 
+{
+    node_type_t type;
+    union
+    {
+        cnode_t cnode;
+        tnode_t tnode;
+        lnode_t lnode;
+    } node;
+};
+
---
+++ code/src/main.c
@@ -0,0 +1,6 @@
+#include "nodes.h"
+
+int main()
+{
+    return 0;
+}
