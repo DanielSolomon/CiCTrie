dd088da52f9825c673226cbbc9c7741bda67f8fa
warforgad <warforgad@gmail.com>
2017-08-18 13:17:06 -0700

    Added NO_PRINT flag

--- code/inc/common.h
+++ code/inc/common.h
@@ -11,12 +11,16 @@
 
 #define MESSAGE_SIZE (4096)
 
+#ifdef NO_PRINT
+#define PRINT(fmt, ...)
+#else
 #define PRINT(fmt, ...) do {                                                                                    \
     char __BUFFER[MESSAGE_SIZE + 1] = {0};                                                                      \
     snprintf(__BUFFER, MESSAGE_SIZE, "[%d] [%s: %d] %s.\n", (int) syscall(SYS_gettid), __FILE__, __LINE__, fmt);\
     printf(__BUFFER, ##__VA_ARGS__);                                                                            \
     fflush(stdout);                                                                                             \
 } while (0)   
+#endif
 
 #define FAIL(fmt, ...) do {     \
     PRINT(fmt, ##__VA_ARGS__);  \
