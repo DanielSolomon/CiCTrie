6b4f8df77895653459c661c5820bd3ed35fe63f7
DanielSolomon <DanielSolomon94.ds@gmail.com>
2017-08-18 16:13:11 +0300

    Added action struct to support any action (lookup, insert and remove) for test to come.

--- code/inc/parser.h
+++ code/inc/parser.h
@@ -1,30 +1,59 @@
 #pragma once
 
-typedef struct {
+typedef enum 
+{
+    LOOKUP,
+    INSERT,
+    REMOVE,
+} action_type_t;
+
+typedef struct 
+{
     int key;
 } lookup_t;
 
-typedef struct {
+typedef struct 
+{
     int      n;
     lookup_t lookups[];
 } lookups_t;
 
-typedef struct {
+typedef struct 
+{
     int key;
     int value;
 } insert_t;
 
-typedef struct {
+typedef struct 
+{
     int      n;
     insert_t inserts[];
 } inserts_t;
 
-typedef struct {
+typedef struct 
+{
     int key;
 } remove_t;
 
-typedef struct {
+typedef struct 
+{
     int      n;
     remove_t removes[];
 } removes_t;
 
+typedef struct 
+{
+    action_type_t type;
+    union 
+    {
+        lookup_t lookup;
+        insert_t insert;
+        remove_t remove;
+    } action;
+} action_t;
+
+typedef struct 
+{
+    int      n;
+    action_t actions[];
+} actions_t;
