e74def8507f104e7aa3e9d7935273b201bebb2af
DanielSolomon <DanielSolomon94.ds@gmail.com>
2017-08-19 23:28:44 +0300

    Now generate numbers, generates insert, lookup and remove samples.

--- code/scripts/generate_numbers.py
+++ code/scripts/generate_numbers.py
@@ -2,13 +2,30 @@ import struct
 import random
 import sys
 
+def generate_inserts(range_, num):
+    integers    = [random.randint(0, range_) for _ in xrange(2 * num)]
+    data        = struct.pack('<I' + 'I' * (2 * num), num, *integers)
+    return data, integers
+
+def generate_lookups(set_, num):
+    integers    = [random.choice(set_) for _ in xrange(num)]
+    data        = struct.pack('<I' + 'I' * num, num, *integers)
+    return data, integers
+
+def generate_removes(set_, num):
+    integers    = [random.choice(set_) for _ in xrange(num)]
+    data        = struct.pack('<I' + 'I' * num, num, *integers)
+    return data, integers
 
 def main(range_, num):
-    # TODO seed.
-    integers    = [random.randint(0, range_) for x in xrange(2 * num)]
-    data        = struct.pack('<I' + 'I' * (2 * num), num, *integers)
-    with open('sample.bin', 'wb') as writer:
+    random.seed(0)
+    data, integers = generate_inserts(range_, num)
+    with open('inserts_sample.bin', 'wb') as writer:
         writer.write(data)
+    with open('lookups_sample.bin', 'wb') as writer:
+        writer.write(generate_lookups(integers, num)[0])
+    with open('removes_sample.bin', 'wb') as writer:
+        writer.write(generate_removes(integers, num)[0])
 
 if __name__ == '__main__':
     main(int(sys.argv[1]), int(sys.argv[2]))
