f7720fb846c2d2b13282b9d3ca5d0f7fb42f42e4
warforgad <warforgad@gmail.com>
2017-08-23 12:36:31 +0300

    Added lookup and remove to scala and complied with our c hash (division by 10)

--- code/scala/benchmark/src/main/scala/Main.scala
+++ code/scala/benchmark/src/main/scala/Main.scala
@@ -1,53 +1,163 @@
 import java.nio.file.{Files, Paths}
 import java.nio.{ByteBuffer, ByteOrder}
 import scala.collection.concurrent.TrieMap
+import scala.collection.mutable.ListBuffer
+
+class MyInt (val x: Int){
+    override def hashCode() = {x/10}
+    override def equals(o: Any) = o match {
+        case that : MyInt => (x == that.x)
+        case _ => false
+    }
+    override def toString() = x.toString
+}
 
 object Constants {
     val NUM_OF_THREADS = 4
 }
 
-class Op(val key:Int, val value:Int) {
-    def print() {
-        println("op " + key + " - " + value)
+trait Op[Key, Value] {
+    def execute(m: TrieMap[Key, Value])
+}
+
+class InsertOp[Key, Value] (key:Key, value:Value) extends Op[Key, Value]{
+    override def execute(m: TrieMap[Key, Value]) {
+        //println("inserting " +key.toString +":"+value)
+        m(key) = value
     }
 }
 
-class WorkerThread(m: TrieMap[Int, Int], ops: Array[Op]) extends Runnable {
+class LookupOp[Key, Value] (key:Key) extends Op[Key, Value] {
+    override def execute(m: TrieMap[Key, Value]) {
+        try {
+            val value = m(key)
+            //println("got " + value + " for " + key)
+        } 
+        catch {
+        case e : Exception => println("got exception for " + key)
+        }
+    }
+}
+
+class RemoveOp[Key, Value] (key:Key) extends Op[Key, Value] {
+    override def execute(m: TrieMap[Key, Value]) {
+        try {
+            m.remove(key)
+            //println("removed " + key)
+        } 
+        catch {
+        case e : Exception => println("got exception for " + key)
+        }
+    }
+}
+
+class WorkerThread[Key, Value](m: TrieMap[Key, Value], ops: Array[Op[Key, Value]]) extends Runnable {
     def run() {
         for (op <- ops) {
-            m(op.key) = op.value
+            op.execute(m)
         }
     }
 }
 
 
 object Main {
+    def handleInsert (map: TrieMap[MyInt, Int], path: String) = {
+        // Read the data and wrap it with a little endian buffer
+        println("Reading data from" + path)
+        val data = Files.readAllBytes(Paths.get(path))
+        val wrapper = ByteBuffer.wrap(data)
+        wrapper.order(ByteOrder.LITTLE_ENDIAN);
+
+        val size = wrapper.getInt
+        val chunk = size / Constants.NUM_OF_THREADS
+        val index = 0
+
+        val threads = new ListBuffer[Thread]()
+
+        for (i <- 1 to Constants.NUM_OF_THREADS) {
+            val arr = new Array[Op[MyInt, Int]](chunk)
+            for (j <- 0 to chunk - 1) {
+                val key = new MyInt(wrapper.getInt)
+                val value = wrapper.getInt
+                arr(j) = new InsertOp[MyInt, Int](key, value)
+            }
+            threads +=  new Thread(new WorkerThread[MyInt, Int](map, arr))
+        }
+        threads.foreach(t => t.start())
+        threads.foreach(t => t.join())
+        println("Done insert");
+    }
+
+    def handleLookup (map: TrieMap[MyInt, Int], path: String) = {
+        // Read the data and wrap it with a little endian buffer
+        println("Reading data from" + path)
+        val data = Files.readAllBytes(Paths.get(path))
+        val wrapper = ByteBuffer.wrap(data)
+        wrapper.order(ByteOrder.LITTLE_ENDIAN);
+
+        val size = wrapper.getInt
+        val chunk = size / Constants.NUM_OF_THREADS
+        val index = 0
+
+        val threads = new ListBuffer[Thread]()
+
+        for (i <- 1 to Constants.NUM_OF_THREADS) {
+            val arr = new Array[Op[MyInt, Int]](chunk)
+            for (j <- 0 to chunk - 1) {
+                val key = new MyInt(wrapper.getInt)
+                arr(j) = new LookupOp[MyInt, Int](key)
+            }
+            threads +=  new Thread(new WorkerThread[MyInt, Int](map, arr))
+        }
+        threads.foreach(t => t.start())
+        threads.foreach(t => t.join())
+        println("Done lookup");
+    }
+
+    def handleRemove (map: TrieMap[MyInt, Int], path: String) = {
+        // Read the data and wrap it with a little endian buffer
+        println("Reading data from" + path)
+        val data = Files.readAllBytes(Paths.get(path))
+        val wrapper = ByteBuffer.wrap(data)
+        wrapper.order(ByteOrder.LITTLE_ENDIAN);
+
+        val size = wrapper.getInt
+        val chunk = size / Constants.NUM_OF_THREADS
+        val index = 0
+
+        val threads = new ListBuffer[Thread]()
+
+        for (i <- 1 to Constants.NUM_OF_THREADS) {
+            val arr = new Array[Op[MyInt, Int]](chunk)
+            for (j <- 0 to chunk - 1) {
+                val key = new MyInt(wrapper.getInt)
+                arr(j) = new RemoveOp[MyInt, Int](key)
+            }
+            threads +=  new Thread(new WorkerThread[MyInt, Int](map, arr))
+        }
+        threads.foreach(t => t.start())
+        threads.foreach(t => t.join())
+        println("Done remove");
+    }
+
     def main(args: Array[String]) = {
         if (args.length == 0) {
             println("Please enter a sample path")
         }
         else {
-            val map = new TrieMap[Int, Int]
+            val map = new TrieMap[MyInt, Int]()
 
-                // Read the data and wrap it with a little endian buffer
-                println("Reading data from" + args(0))
-                val data = Files.readAllBytes(Paths.get(args(0)))
-                val wrapper = ByteBuffer.wrap(data)
-                wrapper.order(ByteOrder.LITTLE_ENDIAN);
-
-            val size = wrapper.getInt
-                val chunk = size / Constants.NUM_OF_THREADS
-                val index = 0
-
-                for (i <- 1 to Constants.NUM_OF_THREADS) {
-                    val arr = new Array[Op](chunk)
-                        for (j <- 0 to chunk - 1) {
-                            val key = wrapper.getInt
-                                val value = wrapper.getInt
-                                arr(j) = new Op(key, value)
-                        }
-                    new Thread(new WorkerThread(map, arr)).start()
+            for (i <- 0 to args.length - 1 by 2) {
+                if (args(i) == "insert") {
+                    handleInsert(map, args(i+1))
                 }
+                if (args(i) == "lookup") {
+                    handleLookup(map, args(i+1))
+                }
+                if (args(i) == "remove") {
+                    handleRemove(map, args(i+1))
+                }
+            }
         }
     }
 }
